691fe05d48c433da3116e759a8d1f5532f069d7c
xpomae <somebodyfell@gmail.com>
2026-06-24 21:29:41 +0200

    Fixed swapping same sign spins

--- src/dynamics/creutz_kawasaki.rs
+++ src/dynamics/creutz_kawasaki.rs
@@ -35,6 +35,25 @@ impl CreutzKawasakiDynamics {
             for x in (0..model.l - 1).step_by(2) {
                 let idx1 = y * model.l + x;
                 let idx2 = idx1 + 1;
+                if model.lattice[idx1] == model.lattice[idx2] {
+                    continue;
+                }
+
+                let energy_delta = model.swap_energy_delta(idx1, idx2);
+                if energy_delta <= self.demons_h[idx1] {
+                    model.swap(idx1, idx2, energy_delta);
+                    self.demons_h[idx1] -= energy_delta;
+                }
+            }
+        }
+        // Horizontal odd
+        for y in 0..model.l {
+            for x in (1..model.l - 1).step_by(2) {
+                let idx1 = y * model.l + x;
+                let idx2 = idx1 + 1;
+                if model.lattice[idx1] == model.lattice[idx2] {
+                    continue;
+                }
 
                 let energy_delta = model.swap_energy_delta(idx1, idx2);
                 if energy_delta <= self.demons_h[idx1] {
@@ -48,6 +67,9 @@ impl CreutzKawasakiDynamics {
             for x in 0..model.l {
                 let idx1 = y * model.l + x;
                 let idx2 = if y == model.l - 1 { x } else { idx1 + model.l };
+                if model.lattice[idx1] == model.lattice[idx2] {
+                    continue;
+                }
 
                 let energy_delta = model.swap_energy_delta(idx1, idx2);
                 if energy_delta <= self.demons_v[idx1] {
@@ -56,24 +78,14 @@ impl CreutzKawasakiDynamics {
                 }
             }
         }
-        // Horizontal odd
-        for y in 0..model.l {
-            for x in (1..model.l - 1).step_by(2) {
-                let idx1 = y * model.l + x;
-                let idx2 = idx1 + 1;
-
-                let energy_delta = model.swap_energy_delta(idx1, idx2);
-                if energy_delta <= self.demons_h[idx1] {
-                    model.swap(idx1, idx2, energy_delta);
-                    self.demons_h[idx1] -= energy_delta;
-                }
-            }
-        }
         // Vertical odd
         for y in (1..model.l).step_by(2) {
             for x in 0..model.l {
                 let idx1 = y * model.l + x;
                 let idx2 = if y == model.l - 1 { x } else { idx1 + model.l };
+                if model.lattice[idx1] == model.lattice[idx2] {
+                    continue;
+                }
 
                 let energy_delta = model.swap_energy_delta(idx1, idx2);
                 if energy_delta <= self.demons_v[idx1] {
@@ -88,9 +100,13 @@ impl CreutzKawasakiDynamics {
         for i in 0..model.l {
             let idx_l = i * model.l;
             let idx_r = (i + 1) * model.l - 1;
-            
-            let flipped_l = self.rng.random_bool((1.0 + model.lattice[idx_l] as f64 * self.m) / 2.0);
-            let flipped_r = self.rng.random_bool((1.0 - model.lattice[idx_r] as f64 * self.m) / 2.0);
+
+            let flipped_l = self
+                .rng
+                .random_bool((1.0 + model.lattice[idx_l] as f64 * self.m) / 2.0);
+            let flipped_r = self
+                .rng
+                .random_bool((1.0 - model.lattice[idx_r] as f64 * self.m) / 2.0);
 
             if flipped_l {
                 model.flip(idx_l, model.flip_energy_delta(idx_l));
