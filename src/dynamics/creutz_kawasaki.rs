ba95d15c49c69d5ce69ce0fe86fc6966d0f73cb5
xpomae <somebodyfell@gmail.com>
2026-06-24 20:49:47 +0200

    Implement CreutzKawasaki sweep()

--- src/dynamics/creutz_kawasaki.rs
+++ src/dynamics/creutz_kawasaki.rs
@@ -1,9 +1,13 @@
 use crate::ising_model::IsingModel;
+use rand::prelude::*;
+use rand::rngs::SmallRng;
 
 pub struct CreutzKawasakiDynamics {
     pub m: f64,
     pub demons_h: Vec<i32>,
     pub demons_v: Vec<i32>,
+
+    rng: SmallRng,
 }
 
 impl CreutzKawasakiDynamics {
@@ -15,44 +19,91 @@ impl CreutzKawasakiDynamics {
             demons_v.push(starting_energy);
         }
 
-        return Self { m, demons_h, demons_v };
+        let rng: SmallRng = rand::make_rng();
+
+        return Self {
+            m,
+            demons_h,
+            demons_v,
+            rng,
+        };
     }
 
     fn process_bonds(&mut self, model: &mut IsingModel) {
+        // Horizontal even
         for y in 0..model.l {
             for x in (0..model.l - 1).step_by(2) {
                 let idx1 = y * model.l + x;
                 let idx2 = idx1 + 1;
 
+                let energy_delta = model.swap_energy_delta(idx1, idx2);
+                if energy_delta <= self.demons_h[idx1] {
+                    model.swap(idx1, idx2, energy_delta);
+                    self.demons_h[idx1] -= energy_delta;
+                }
             }
         }
-        
-        for y in 0..model.l {
-            for x in (1..model.l - 1).step_by(2) {
-                let idx1 = y * model.l + x;
-                let idx2 = idx1 + 1;
-                
-            }
-        }
-        
+        // Vertical even
         for y in (0..model.l).step_by(2) {
             for x in 0..model.l {
                 let idx1 = y * model.l + x;
                 let idx2 = if y == model.l - 1 { x } else { idx1 + model.l };
-                
+
+                let energy_delta = model.swap_energy_delta(idx1, idx2);
+                if energy_delta <= self.demons_v[idx1] {
+                    model.swap(idx1, idx2, energy_delta);
+                    self.demons_v[idx1] -= energy_delta;
+                }
             }
         }
-        
+        // Horizontal odd
+        for y in 0..model.l {
+            for x in (1..model.l - 1).step_by(2) {
+                let idx1 = y * model.l + x;
+                let idx2 = idx1 + 1;
+
+                let energy_delta = model.swap_energy_delta(idx1, idx2);
+                if energy_delta <= self.demons_h[idx1] {
+                    model.swap(idx1, idx2, energy_delta);
+                    self.demons_h[idx1] -= energy_delta;
+                }
+            }
+        }
+        // Vertical odd
         for y in (1..model.l).step_by(2) {
             for x in 0..model.l {
                 let idx1 = y * model.l + x;
                 let idx2 = if y == model.l - 1 { x } else { idx1 + model.l };
-                
+
+                let energy_delta = model.swap_energy_delta(idx1, idx2);
+                if energy_delta <= self.demons_v[idx1] {
+                    model.swap(idx1, idx2, energy_delta);
+                    self.demons_v[idx1] -= energy_delta;
+                }
             }
         }
     }
 
-    fn process_reservoirs(&mut self, model: &mut IsingModel) {}
+    fn process_reservoirs(&mut self, model: &mut IsingModel) {
+        for i in 0..model.l {
+            let idx_l = i * model.l;
+            let idx_r = (i + 1) * model.l - 1;
+
+            let flipped_l = self
+                .rng
+                .random_bool(f64::midpoint(1.0, model.lattice[idx_l] as f64 * self.m));
+            let flipped_r = self
+                .rng
+                .random_bool(f64::midpoint(1.0, -model.lattice[idx_r] as f64 * self.m));
+
+            if flipped_l {
+                model.flip(idx_l, model.flip_energy_delta(idx_l));
+            }
+            if flipped_r {
+                model.flip(idx_r, model.flip_energy_delta(idx_r));
+            }
+        }
+    }
 
     pub fn sweep(&mut self, model: &mut IsingModel) {
         self.process_bonds(model);
